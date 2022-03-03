e3ae3b62314cd7cf6ace88aceb0d8cbdb6dad4c8
Michele Picozzi <michele.picozzi@hotmail.it>
2022-03-03 11:02:34 +0100

    crating smart contracts

---
+++ contracts/NFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    // allows to keep track of the id of each token once minted (1, 2, 3 etc)
+    Counters.Counter private _tokenIds;
+    //
+    address contractAddress;
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "121668fd281240e880029c7bb44579b0"
 
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnnet:{
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
